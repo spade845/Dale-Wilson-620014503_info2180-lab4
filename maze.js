75f7cd03b4781011555e6ee1173a585c40601bec
spade845 <spade845@hotmail.com>
2016-10-15 21:31:41 -0500

    4rth commit

--- maze.js
+++ maze.js
@@ -3,15 +3,18 @@ Dale Wilson 620014503
 lab4
 */
 
-window.onload =function eventhandling(){
+window.onload =function eventhandling() {
 
+	var start =document.getElementById('start');
 	var win =document.getElementById('end');
-	var wallsOfmaze =document.querySelectorAll('#maze div.boundary');
+	var wallsOfMaze =document.querySelectorAll('div.boundary');
+
+	start.setAttribute('onclick','init()');
 /*
 changes colour of div
 */
 	for (var i =0; i <=4; i++) {
-		wallsOfmaze[i].setAttribute('onmouseover','test()');
+		wallsOfMaze[i].setAttribute('onmouseover','test()');
 	}
 
 	win.setAttribute('onmouseover','winn()');
@@ -20,21 +23,29 @@ changes colour of div
 function test() {
 	
 	alert('You Lose!');
-	var wallsOfmaze =document.querySelectorAll('div.boundary');
+	var wallsOfMaze=document.querySelectorAll('div.boundary');
 /*
 changes colour of div
 */
 	for (var i =0; i <=4; i++) {
-		wallsOfmaze[i].setAttribute('style','background-color:#ff8888');
+		wallsOfMaze[i].setAttribute('style','background-color:#ff8888');
 	}	
 }
 
 function winn() {
 
 	if(document.getElementById('boundary1').getAttribute('style') == 'background-color:#ff8888') {
-		alert('You must go through the maze \nwithout hitting the walls!');
+		alert('Avoid the walls');
 	}	
 	else {
 		alert('You Win!');
+	}
+}
+
+function init() {
+	var wallsOfMaze =document.querySelectorAll('div.boundary');
+
+	for (var i =0; i <=4; i++) {
+		wallsOfMaze[i].setAttribute('style','background-color: #eeeeee');
 	}
 }
