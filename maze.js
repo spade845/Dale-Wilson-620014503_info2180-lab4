4c460d9e8b6155999d0a6425d05d8e98f5adf269
spade845 <spade845@hotmail.com>
2016-10-15 20:35:53 -0500

    first commit
    first test change color

---
+++ maze.js
@@ -0,0 +1,25 @@
+/*
+Dale Wilson 620014503
+*/
+
+
+
+window.onload=function eventhandling() {
+
+	var wallsOfMaze=document.getElementsByTagName('div');
+	
+	
+/*
+loop to select each div
+*/
+	for (var i =2; i <=6; i++) {
+		wallsOfMaze[i].setAttribute('onmouseover','test(this)');
+	}
+	
+};
+/*
+changes colour of div
+*/
+function test(element) {
+	element.setAttribute('style','background-color:#ff8888');
+}
